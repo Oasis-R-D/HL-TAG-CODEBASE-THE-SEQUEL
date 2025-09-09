27ebcd69634d07071bcb2af8a1fa0b5e31884c4d
Pacman09 <pacmanproductionsz25@gmail.com>
2025-09-09 12:14:46 -0600

    add serverside variable for money

--- dlls/player.h
+++ dlls/player.h
@@ -175,6 +175,7 @@ public:
 	int m_iClientHealth;  // the health currently known by the client.  If this changes, send a new
 	int m_iClientBattery; // the Battery currently known by the client.  If this changes, send a new
 	int m_iClientMoney;
+	int m_iServerMoney;
 	int m_iHideHUD;		  // the players hud weapon info is to be hidden
 	int m_iClientHideHUD;
 	int m_iFOV;		  // field of view
